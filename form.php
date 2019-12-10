94dbcce521cff289c142f1114fbde0381d96eea5
Louis-vd <louis.vandriessche.1@gmail.com>
2019-12-10 15:00:04 +0100

    json

--- form.php
+++ form.php
@@ -1,32 +1,48 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1> To Do Liste </h1>
-    <h2>Ajouter une tache</h2>
-    <form method="post" action="index.php" class="input_form">
-		<input type="text" name="task" class="task_input">
-		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
-	</form>
-    <h2>Taches</h2>
-    <form action="#" method="post">
-<input type="checkbox" name="gender" value="task"></input>
+<?php session_start();
 
-
-</form>
-</body>
-</html>
-
-<?php
+function add_to_json(){
 //Sanitisation et validation
-$options = array(
-    'task' 	=> FILTER_SANITIZE_STRING,
-    );
-$result = filter_input_array(INPUT_POST, $options); 
-echo $result['task'];
+
+$task = filter_input(INPUT_POST, 'task', FILTER_SANITIZE_STRING); 
+
+$_SESSION['task'] = $task;
+// echo "<pre>";
+// echo $task;
+
+$new_task = [
+    "task" => $task,
+    "done" => false
+];
+
+$task_json = json_encode($new_task, JSON_PRETTY_PRINT);
+
+$json = 'tasks.json';
+
+file_get_contents($json);
+file_put_contents($json, $task_json . "\n", FILE_APPEND);
+
+
+$test = file($json);
+foreach($test as $task){
+    echo $task;
+}
+
+}
+
+add_to_json();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
